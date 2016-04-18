41c819eafb7e7d19a482f587033ed0c21bca045e
Alexander Balagura <alexanderbalagura@gmail.com>
2016-04-18 11:09:24 +0300

    Test index page articles order

--- cms/article/tests.py
+++ cms/article/tests.py
@@ -12,4 +12,11 @@ class ArticleTest(TestCase):
         # Is page reachable
         self.assertEqual(response.status_code, 200)
         # Is page context contains articles list with 5 elements
-        self.assertEqual(len(response.context.get('articles')), 5)
+        self.assertEqual(len(response.context.get('articles', [])), 5)
+
+        first_article = Article.objects.order_by('-id').first()
+        # Text order
+        first_article.is_published = True
+        first_article.save()
+        response = self.client.get(reverse('article:index'))
+        self.assertIn(first_article, response.context.get('articles', []))

721a268c2c4ce1c9eb8f5773e28476133f06887b
Alexander Balagura <alexanderbalagura@gmail.com>
2016-04-18 08:35:29 +0300

    Add article model

--- cms/article/models.py
+++ cms/article/models.py
@@ -1,5 +1,12 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Article(models.Model):
+    """Article model"""
+    title = models.CharField(_('Article title'), max_length=250)
+    slug = models.SlugField(_('Article slug'), max_length=250)
+    image = models.ImageField(_('Article image'), upload_to='article')
+    content = models.TextField(_('Article content text'))

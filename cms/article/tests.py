87f7ed163a5da5e0f6bc3b485e0d56095eb4a822
Alexander Balagura <alexanderbalagura@gmail.com>
2016-04-18 11:03:59 +0300

    Pass test

--- cms/article/tests.py
+++ cms/article/tests.py
@@ -1,13 +1,15 @@
 from django.test import TestCase
 from django.core.urlresolvers import reverse
 
+from article.models import Article
+
 
 class ArticleTest(TestCase):
-    fixures = ['fixures.json']
+    fixtures = ['all.json']
 
     def test_index(self):
         response = self.client.get(reverse('article:index'))
         # Is page reachable
         self.assertEqual(response.status_code, 200)
         # Is page context contains articles list with 5 elements
-        self.assertEqual(len(response.context.get('articles', [])), 5)
+        self.assertEqual(len(response.context.get('articles')), 5)
--- cms/article/views.py
+++ cms/article/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from django.views.decorators.http import require_GET
 
+from article.models import Article
 
 @require_GET
 def index(request):
     """View for main page"""
-    return render(request, 'article/index.html')
+
+    articles = Article.objects.all()[0:5]
+    return render(
+        request,
+        'article/index.html',
+        {'articles': articles}
+    )

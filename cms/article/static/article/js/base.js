fe53fbc888c02dac4b5b672331ef3251f612a5dc
Alexander Balagura <alexanderbalagura@gmail.com>
2016-04-18 15:49:44 +0300

    Compleat getting next articles by ajax

--- cms/article/static/article/js/base.js
+++ cms/article/static/article/js/base.js
@@ -1,14 +1,20 @@
 (function($){
-    var page = 1,
-        url_pattern =  'paginate/{{page}}';
+    // Listen to .load-more bnt and
+    // append articles links to table from paginate/{{page}}
+    var page = 2,
+        url_pattern = 'paginate/{{page}}';
         $table = $('.table');
+        $btn = $('.btn.load-more');
 
-    $('.load-more').click(function(event){
-        console.log('here!');
+    $btn.click(function(event){
         $.get(url_pattern.replace('{{page}}', page), function(data){
             if(data.length > 0){
                 $table.find('tbody').append(data);
-                page ++;
+                if ($table.find('[data-last]').length > 0){
+                    $btn.addClass('disabled');
+                } else {
+                    page ++;
+                }
             }
         });
     });

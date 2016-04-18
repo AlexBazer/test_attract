c76c62cf7433386bfb6398ff472537db807da61e
Alexander Balagura <alexanderbalagura@gmail.com>
2016-04-18 09:21:02 +0300

    Set up media and static paths

--- cms/cms/settings.py
+++ cms/cms/settings.py
@@ -118,5 +118,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static.auto')
 STATIC_URL = '/static/'
+
+# Media files
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'

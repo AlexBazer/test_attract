af22c48242a3a8a85a45e5c26c64f4be377e99a9
Alexander Balagura <alexanderbalagura@gmail.com>
2016-04-18 10:45:32 +0300

    Add test to count index context articles

--- cms/article/tests.py
+++ cms/article/tests.py
@@ -7,4 +7,7 @@ class ArticleTest(TestCase):
 
     def test_index(self):
         response = self.client.get(reverse('article:index'))
+        # Is page reachable
         self.assertEqual(response.status_code, 200)
+        # Is page context contains articles list with 5 elements
+        self.assertEqual(len(response.context.get('articles', [])), 5)

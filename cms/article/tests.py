0eecf572adb2585a259a9b0bd4c828dc26c6f1f1
Alexander Balagura <alexanderbalagura@gmail.com>
2016-04-18 10:26:17 +0300

    Add test for article index

--- cms/article/tests.py
+++ cms/article/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
+from django.core.urlresolvers import reverse
 
-# Create your tests here.
+
+class ArticleTest(TestCase):
+    fixures = ['fixures.json']
+
+    def test_index(self):
+        response = self.client.get(reverse('article:index'))
+        self.assertEqual(response.status_code, 200)
